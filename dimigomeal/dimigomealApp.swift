c1616365210ce14dcefdba538d1deb0730d2a72c
noViceMin <admin@isamin.kr>
2024-06-12 11:09:18 +0900

    Initial Commit

---
+++ dimigomeal/dimigomealApp.swift
@@ -0,0 +1,17 @@
+//
+//  dimigomealApp.swift
+//  dimigomeal
+//
+//  Created by noViceMin on 2024-06-12.
+//
+
+import SwiftUI
+
+@main
+struct dimigomealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
